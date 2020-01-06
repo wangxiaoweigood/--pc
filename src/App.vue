39b86e5b9740c797f9f2ac3f4f084abed2e3f029
wangxiaowei520 <WaNgWiaOWei>
2020-01-06 17:21:30 +0800

    项目初始化-封装注册Vant组件模块

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-popup v-model="show" position="top" :style="{ height: '20%' }" />
+    </van-cell-group>
+    <van-cell is-link @click="showPopup">展示弹出层</van-cell>
+
+    <van-popup v-model="show">内容</van-popup>
+
     <router-view></router-view>
   </div>
 </template>
---
+++ src/utils/regisster-vant.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import {
+  Popup,
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'
+Vue.use(Popup)
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
